30fc7ef6775c3320316b45ed853b3d8e0053b5e2
Larisa Pyykola <larisa.pyykola@gmail.com>
2022-12-20 14:08:59 +0200

    Added get_all_drones_now logic

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 var convert = require("xml-js");
 
-const getData = async () => {
+let allDronesList;
+
+const nestPositionX = 250000;
+const nestPositionY = 250000;
+const noFlyZoneRadius = 100 * 1000; // radius in units
+
+const getDronesData = async () => {
   const res = await fetch("https://assignments.reaktor.com/birdnest/drones");
   if (res.ok) {
     const data = await res;
@@ -10,16 +16,24 @@ const getData = async () => {
     console.log(convert.xml2json(content, { compact: true, spaces: 4 }));
     const dronesDataJson = convert.xml2json(content, {
       compact: true,
-      spaces: 4,
+      spaces: 2,
     });
-    return dronesDataJson;
+    allDronesList = JSON.parse(dronesDataJson);
   }
 };
 
-getData();
+getDronesData();
 
-app.get("/api/drones", (request, response) => {
-  response.json("test");
+app.get("/api/all_drones_now", (request, response) => {
+  allDronesList.report
+    ? response.json(allDronesList.report.capture.drone)
+    : response.json("data is not ready");
+});
+
+app.get("/api/drones_in_nfz", (request, response) => {
+  dronesInNFZ
+    ? response.json(adronesInNFZ)
+    : response.json("data is not ready");
 });
 
 const PORT = 3001;
